cc1c7b34431c5f7f98d60f0dd2e01cd0748f249d
kietnguyen0211 <kietnguyen0211@gmail.com>
2023-05-18 19:25:36 +0700

    up

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 // https://github.com/kietnguyen0211/MovieApi2.git
+// https://github.com/kietnguyen0211/UpGithub.git
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
